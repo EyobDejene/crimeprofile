bdde89dd1aac2632ae2cf69436c8742263b8ba9a
eyobdejene <eyob_westerink@hotmail.com>
2021-06-04 14:15:34 +0200

    edit fixes

--- js/detect.js
+++ js/detect.js
@@ -230,10 +230,10 @@ function runDetection(state) {
     const video = document.querySelector('#my_camera video');
 
     Promise.all([
-      faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
-      faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
-      faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
-      faceapi.nets.faceExpressionNet.loadFromUri('/models')
+      faceapi.nets.tinyFaceDetector.loadFromUri('models'),
+      faceapi.nets.faceLandmark68Net.loadFromUri('models'),
+      faceapi.nets.faceRecognitionNet.loadFromUri('models'),
+      faceapi.nets.faceExpressionNet.loadFromUri('models')
     ]).then(startVideo);
 
     function startVideo() {
